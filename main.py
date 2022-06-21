18762efca388b209d843fd9c593d13888e3256c8
gnamiro <naeimghahramanpour@gmail.com>
2022-06-21 20:14:48 +0430

    FIX: fix resize problem of widgets inside app

---
+++ main.py
@@ -0,0 +1,176 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os
+# TODO: 1. import logging
+
+
+class Ui_dialog(object):
+    def setupUi(self, dialog):
+        dialog.setObjectName("dialog")
+        dialog.resize(1001, 787)
+        self.gridLayout = QtWidgets.QGridLayout(dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton_3 = QtWidgets.QPushButton(dialog)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.pushButton_3.sizePolicy().hasHeightForWidth())
+
+        self.pushButton_3.setSizePolicy(sizePolicy)
+        self.pushButton_3.setMinimumSize(QtCore.QSize(150, 35))
+        self.pushButton_3.setMaximumSize(QtCore.QSize(150, 39))
+        self.pushButton_3.setBaseSize(QtCore.QSize(150, 35))
+        self.pushButton_3.setStyleSheet("")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.addWidget(self.pushButton_3, 0, 0, 1, 1)
+
+        self.pushButton_3.clicked.connect(self.chooseFolder)
+
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
+        self.verticalLayout_2.setSpacing(18)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+        self.label_2 = QtWidgets.QLabel(dialog)
+        self.label_2.setMaximumSize(QtCore.QSize(100, 20))
+        self.label_2.setStyleSheet("font-weight: 599")
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(
+            self.label_2, 0, QtCore.Qt.AlignRight | QtCore.Qt.AlignTop)
+
+        self.listWidget = QtWidgets.QListWidget(dialog)
+        self.listWidget.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.listWidget.sizePolicy().hasHeightForWidth())
+        self.listWidget.setSizePolicy(sizePolicy)
+        self.listWidget.setMinimumSize(QtCore.QSize(220, 538))
+        self.listWidget.setMaximumSize(QtCore.QSize(256, 16777215))
+        self.listWidget.viewport().setProperty(
+            "cursor", QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.listWidget.setObjectName("listWidget")
+        self.verticalLayout_2.addWidget(
+            self.listWidget, 0, QtCore.Qt.AlignHCenter)
+
+        self.gridLayout.addLayout(self.verticalLayout_2, 2, 2, 3, 1)
+        self.graphicsView = QtWidgets.QGraphicsView(dialog)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(17)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.graphicsView.sizePolicy().hasHeightForWidth())
+        self.graphicsView.setSizePolicy(sizePolicy)
+        self.graphicsView.setMinimumSize(QtCore.QSize(589, 0))
+        self.graphicsView.setMaximumSize(QtCore.QSize(16777215, 160000))
+        self.graphicsView.viewport().setProperty(
+            "cursor", QtGui.QCursor(QtCore.Qt.CrossCursor))
+        self.graphicsView.setObjectName("graphicsView")
+        self.gridLayout.addWidget(self.graphicsView, 2, 0, 4, 2)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setSizeConstraint(
+            QtWidgets.QLayout.SetFixedSize)
+        self.horizontalLayout_3.setSpacing(4)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.pushButton_2 = QtWidgets.QPushButton(dialog)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+
+        sizePolicy.setHeightForWidth(
+            self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setMinimumSize(QtCore.QSize(111, 0))
+        self.pushButton_2.setMaximumSize(QtCore.QSize(162, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_3.addWidget(
+            self.pushButton_2, 0, QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+
+        self.pushButton = QtWidgets.QPushButton(dialog)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMaximumSize(QtCore.QSize(89, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout_3.addWidget(
+            self.pushButton, 0, QtCore.Qt.AlignBottom)
+
+        self.gridLayout.addLayout(self.horizontalLayout_3, 5, 2, 1, 1)
+        self.label = QtWidgets.QLabel(dialog)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setStyleSheet("font-weight: 500")
+        self.label.setObjectName("label")
+
+        self.listWidget.itemClicked.connect(self.showImage)
+
+        self.gridLayout.addWidget(self.label, 0, 2, 1, 1, QtCore.Qt.AlignRight)
+
+        self.retranslateUi(dialog)
+        QtCore.QMetaObject.connectSlotsByName(dialog)
+
+    def retranslateUi(self, dialog):
+        _translate = QtCore.QCoreApplication.translate
+        dialog.setWindowTitle(_translate("dialog", "Dialog"))
+        self.pushButton_3.setText(_translate("dialog", "انتخاب پوشه عکس‌ها"))
+        self.label_2.setText(_translate("dialog", "پوشه عکس‌ها"))
+        self.pushButton_2.setText(_translate("dialog", "ذخیره"))
+        self.pushButton.setText(_translate("dialog", "لغو"))
+        self.label.setText(_translate(
+            "dialog", "برنامه تعیین ناهنجاری در عکس‌های مربوط به چشم"))
+
+    def chooseFolder(self):
+        self.dir = QtWidgets.QFileDialog.getExistingDirectory(
+            None, 'Select a folder:', 'C:\\', QtWidgets.QFileDialog.ShowDirsOnly)
+        if self.dir != '':
+            self.readFolder(self.dir)
+
+    def readFolder(self, folder):
+        self.images = []
+        files = os.listdir(folder)
+        # TODO: 2. this method don't recursively search in whole directories inside chosen directory.
+        for file in files:
+            if isAnImage(file):
+                self.images.append(file)
+
+        self.importInsideListWidget(self.images)
+
+    def importInsideListWidget(self, images):
+        self.listWidget.clear()
+        self.listWidget.addItems(images)
+        print(images)
+
+    def showImage(self):
+        print(self.listWidget.currentItem().text())
+        pass
+
+
+def isAnImage(fileName):
+    if fileName.endswith('.png') or fileName.endswith('.jpg') or fileName.endswith('.jpeg'):
+        return True
+
+    return False
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = QtWidgets.QDialog()
+    ui = Ui_dialog()
+    ui.setupUi(dialog)
+    dialog.show()
+    sys.exit(app.exec_())
